d8a0c65766bf9791d71d10e568728c48fd75191e
Mohd Sakib Shaikh <mohd.sakib.shaikh@ibm.com>
2024-11-06 16:24:00 +0530

    created quiz app

---
+++ src/main/java/com/example/quizApp/controller/QuizController.java
@@ -0,0 +1,36 @@
+package com.example.quizApp.controller;
+
+
+import com.example.quizApp.model.QuestionWrapper;
+import com.example.quizApp.model.Response;
+import com.example.quizApp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+// controller for quiz
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+
+}
